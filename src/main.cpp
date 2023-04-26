51e6d64b19cbd6c886f8a0a9d53c95401d845119
4t-2 <zbfddx@protonmail.com>
2023-04-26 22:33:53 +0100

    Added text typing

--- src/main.cpp
+++ src/main.cpp
@@ -39,17 +39,70 @@ int main()
 	rectangle.setPosition({100, 100});
 	rectangle.setSize({1720, 880});
 
+	agl::Font font;
+	font.setup("/usr/share/fonts/TTF/Arial.TTF", 24);
+
+	agl::Text text;
+	text.setColor(agl::Color::White);
+	text.setPosition({0, 0});
+	text.setFont(&font);
+	text.setScale(1);
+
+	std::string str = "";
+
 	while (!event.windowClose())
 	{
 		event.poll();
 
 		window.clear();
 
-		window.drawShape(rectangle);
-		
+		window.drawText(text);
+
 		window.display();
+
+		// also lazy fix, cant be bothered to do utf8 yet
+
+		bool utf8 = false;
+
+		for (const char &byte : event.keybuffer)
+		{
+			if (byte & (1 << 8))
+			{
+				utf8 = true;
+				break;
+			}
+		}
+
+		if (!utf8)
+		{
+			str += event.keybuffer;
+
+			while (str[0] == 127 || str[0] == 8)
+			{
+				str.erase(0, 1);
+			}
+
+			for (int i = 1; i < str.length(); i++)
+			{
+				if (str[i] == 13)
+				{
+					str[i] = 10;
+				}
+
+				if (str[i] == 127 || str[i] == 8)
+				{
+					str.erase(i - 1, 2);
+					i--;
+				}
+			}
+		}
+
+		text.clearText();
+		text.setText(str);
 	}
 
+	text.clearText();
+	font.deleteFont();
 	blank.deleteTexture();
 	shader.deleteProgram();
 	window.close();
